497a6c05ed0fee67b72f013d167035834fbb7381
Bati Kadir <131696253+Bati1K@users.noreply.github.com>
2024-02-08 21:35:02 -0600

    comment added

--- WebScraper/webscraper.py
+++ WebScraper/webscraper.py
@@ -1,14 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # Import BeautifulSoup for parsing HTML
 
-
+# URL of the webpage to scrape
 url = "https://www.frontendmentor.io/challenges"
 
+# Send an HTTP GET request to the URL and get the response
 response = requests.get(url)
 
-html = response.content
-soup = BeautifulSoup(html, 'html.parser')
+html = response.content # Get the content of the response
+soup = BeautifulSoup(html, 'html.parser') # Parse the HTML content using BeautifulSoup
 
+
+# Find all posts on the webpage with the specified class
 posts = soup.find_all('div', class_="Content__Wrapper-sc-f0243o-0 lmXsSl")
 
 for post in posts:
